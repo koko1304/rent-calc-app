691cd9c39ca210242a212652599ca39fe37c9240
Ratna <tensovannratna.study@gmail.com>
2020-02-07 13:00:15 +0700

    Created Design

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,8 +3,79 @@ import React from "react";
 const App = () => {
 	return (
 		<div className="container">
-			<div className="text-center mt-5">
-				<h1>Hello, Mr Ratna</h1>
+			<div className="jumbotron mt-5">
+				<h1 className="display-4">Rent Bill Calculation</h1>
+				<p>
+					<b>Electric Use:</b>
+				</p>
+				<div className="row">
+					<div className="col-md-6">
+						<div className="input-group mb-3">
+							<div className="input-group-prepend">
+								<span className="input-group-text">
+									New Amount
+								</span>
+							</div>
+							<input
+								type="number"
+								className="form-control"
+								placeholder="Number"
+							/>
+						</div>
+					</div>
+					<div className="col-md-6">
+						<div className="input-group mb-3">
+							<div className="input-group-prepend">
+								<span className="input-group-text">
+									Old Amount
+								</span>
+							</div>
+							<input
+								type="number"
+								className="form-control"
+								placeholder="Number"
+							/>
+						</div>
+					</div>
+				</div>
+				<p><b>Water Use:</b></p>
+				<div className="row">
+					<div className="col-md-6">
+						<div className="input-group mb-3">
+							<div className="input-group-prepend">
+								<span className="input-group-text">
+									New Amount
+								</span>
+							</div>
+							<input
+								type="number"
+								className="form-control"
+								placeholder="Number"
+							/>
+						</div>
+					</div>
+					<div className="col-md-6">
+						<div className="input-group mb-3">
+							<div className="input-group-prepend">
+								<span className="input-group-text">
+									Old Amount
+								</span>
+							</div>
+							<input
+								type="number"
+								className="form-control"
+								placeholder="Number"
+							/>
+						</div>
+					</div>
+				</div>
+				<hr className="my-4" />
+				<p>
+					0.0R
+				</p>
+				<a className="btn btn-primary btn-lg" href="#" role="button">
+					Calculation
+				</a>
 			</div>
 		</div>
 	);
